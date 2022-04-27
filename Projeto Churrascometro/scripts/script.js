fb03475a4b1d0d4f7209ffd3fb108d7d63fbac07
arthurpxx <fdtuca@gmail.com>
2022-04-27 16:41:50 -0300

    churrascometro quase finalizado

--- Projeto Churrascometro/scripts/script.js
+++ Projeto Churrascometro/scripts/script.js
@@ -2,3 +2,37 @@
 // Cerveja - 1200ml por pessoa + 6 horas 2000ml
 // Refrigerante - 1000ml por pessoa + 6 horas 1500ml
 // Criança valem por 0,5
+
+let inputAdulto = document.getElementById("adulto")
+let inputCrianca = document.getElementById("crianca")
+let inputDuracao = document.getElementById("duracao")
+
+let resultado = document.getElementById("resultado")
+
+function calcular() {
+    console.log("calculando");
+
+    let adulto = inputAdulto.value
+    let crianca = inputCrianca.value
+    let duracao = inputDuracao.value
+
+    let qtdTotalCarne = carnePP(duracao) * adulto + (carnePP(duracao) / 2 * crianca)
+    let qtdTotalCerveja = cervejaPP(duracao) * adulto
+    let qtdTotalBebida = bebidaPP(duracao) * adulto + (bebidaPP(duracao) / 2 * crianca);
+}
+
+function carnePP(duracao) {
+    if (duracao >= 6) {
+        return 650
+    } else {
+        return 400
+    }
+}
+
+function cervejaPP(duracao) {
+    if (duracao >= 6) {
+        return 2000
+    } else {
+        return 1200
+    }
+}
