f71be19644a82bcb245c746457d0664896b43d51
arthurpxx <fdtuca@gmail.com>
2022-04-28 16:56:17 -0300

    Iniciando HTML avançado

---
+++ HTML-avançado/Atributos Personalizados/Parte 2/script.js
@@ -0,0 +1,11 @@
+let lista = document.getElementById("lista")
+lista.dataset.num = "4"
+let num = parseInt(lista.dataset.num)
+
+let conteudo = ""
+
+for (let i = 0; i < num; i++) {
+    conteudo += "<li>" + i + "</li>"
+}
+
+lista.innerHTML = conteudo
